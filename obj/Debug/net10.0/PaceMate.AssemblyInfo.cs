0dd492566f46a5e883fccc1cfe70948e13bd0d38
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2026-01-07 15:51:37 -0500

    Moved the map initialization to javascript. Added live marker updates based on location.

--- obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
+++ obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a21640d1859d576e784aab162d57d86fa178d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be6199975407b8bf1a0720726ae9769579f16f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
