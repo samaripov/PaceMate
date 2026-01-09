93910487d7bc95167db895e1c767f2293367a59c
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2026-01-08 20:35:19 -0500

    Polling user location ever second instead of 100ms

--- obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
+++ obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+331b197cd473bf0cfe8059095e4004778c876bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f7aae6cff86068434952266a27eecc4351d2c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net10.0/PaceMate.AssemblyInfo.cs
+++ obj/Release/net10.0/PaceMate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f31c6f956eba5e68c5b8f276f69dd2e04c0eabd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f7aae6cff86068434952266a27eecc4351d2c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
