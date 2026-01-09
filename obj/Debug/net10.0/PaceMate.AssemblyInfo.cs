5c3fd4987ea6e3819c256282c878998547208788
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2026-01-08 19:29:59 -0500

    Fixed the distance calculation function.

--- obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
+++ obj/Debug/net10.0/PaceMate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190428bae30ef708c19f2e618714b7c575d563de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cd41b783eda4d138752eeef94dd3bfde0fa539")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaceMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
